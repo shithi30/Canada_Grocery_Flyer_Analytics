108838b599467a4da7f962ff92798e65ce49be90
Shithi Maitra <shithi30@gmail.com>
2024-08-21 11:01:25 -0400

    Update Scrape Flyers to BigQuery.py

--- Scrape Flyers to BigQuery.py
+++ Scrape Flyers to BigQuery.py
@@ -110,21 +110,21 @@ def scrape_flyer(site, url, element):
     return flyer_df
     
 ## caller
-@fuckit
+# @fuckit
 def scrape_call():
 
     # call
     flyer_df = pd.DataFrame()
-    flyer_df = pd.concat([flyer_df, scrape_flyer("NoFrills", "https://www.nofrills.ca/print-flyer?navid=flyout-L2-Flyer", "button")], ignore_index = True)
-    flyer_df = pd.concat([flyer_df, scrape_flyer("Freshco", "https://freshco.com/flyer", "button")], ignore_index = True)
-    flyer_df = pd.concat([flyer_df, scrape_flyer("ThriftyFoods", "https://www.thriftyfoods.com/weekly-flyer", "button")], ignore_index = True)
-    flyer_df = pd.concat([flyer_df, scrape_flyer("Foodland", "https://foodland.ca/flyer", "button")], ignore_index = True)
+    # flyer_df = pd.concat([flyer_df, scrape_flyer("NoFrills", "https://www.nofrills.ca/print-flyer?navid=flyout-L2-Flyer", "button")], ignore_index = True)
+    # flyer_df = pd.concat([flyer_df, scrape_flyer("Freshco", "https://freshco.com/flyer", "button")], ignore_index = True)
+    # flyer_df = pd.concat([flyer_df, scrape_flyer("ThriftyFoods", "https://www.thriftyfoods.com/weekly-flyer", "button")], ignore_index = True)
+    # flyer_df = pd.concat([flyer_df, scrape_flyer("Foodland", "https://foodland.ca/flyer", "button")], ignore_index = True)
     flyer_df = pd.concat([flyer_df, scrape_flyer("Sobeys", "https://www.sobeys.com/en/flyer", "button")], ignore_index = True)
-    flyer_df = pd.concat([flyer_df, scrape_flyer("Safeway", "https://www.safeway.ca/flyer", "button")], ignore_index = True)
-    flyer_df = pd.concat([flyer_df, scrape_flyer("IGA", "https://www.iga.net/en/flyer", "a")], ignore_index = True)
-    flyer_df = pd.concat([flyer_df, scrape_flyer("RealCanadian", "https://www.realcanadiansuperstore.ca/print-flyer?navid=flyout-L2-Flyer", "button")], ignore_index = True)
-    flyer_df = pd.concat([flyer_df, scrape_flyer("Loblaws", "https://www.loblaws.ca/print-flyer?navid=flyout-L2-Flyer", "button")], ignore_index = True)
-    flyer_df = pd.concat([flyer_df, scrape_flyer("Zehrs", "https://www.zehrs.ca/print-flyer?navid=flyout-L2-Flyer", "a")], ignore_index = True)
+    # flyer_df = pd.concat([flyer_df, scrape_flyer("Safeway", "https://www.safeway.ca/flyer", "button")], ignore_index = True)
+    # flyer_df = pd.concat([flyer_df, scrape_flyer("IGA", "https://www.iga.net/en/flyer", "a")], ignore_index = True)
+    # flyer_df = pd.concat([flyer_df, scrape_flyer("RealCanadian", "https://www.realcanadiansuperstore.ca/print-flyer?navid=flyout-L2-Flyer", "button")], ignore_index = True)
+    # flyer_df = pd.concat([flyer_df, scrape_flyer("Loblaws", "https://www.loblaws.ca/print-flyer?navid=flyout-L2-Flyer", "button")], ignore_index = True)
+    # flyer_df = pd.concat([flyer_df, scrape_flyer("Zehrs", "https://www.zehrs.ca/print-flyer?navid=flyout-L2-Flyer", "a")], ignore_index = True)
     
     # error
     to_scraped = set(["NoFrills", "Sobeys", "Freshco", "ThriftyFoods", "Foodland", "Safeway", "IGA", "RealCanadian", "Loblaws", "Zehrs"])
@@ -140,22 +140,22 @@ flyer_df = scrape_call()
 print("\nTotal flyer items: " + str(flyer_df.shape[0]))
 driver.close()
 
-# BigQuery
-credentials = service_account.Credentials.from_service_account_info(json.loads(os.getenv("BIGQUERY_KEYS_JSON")))
-client = bigquery.Client(credentials = credentials, project = credentials.project_id)
-dataset, table = "dbt_smaitra", "landing_grocery_flyer_items"
+# # BigQuery
+# credentials = service_account.Credentials.from_service_account_info(json.loads(os.getenv("BIGQUERY_KEYS_JSON")))
+# client = bigquery.Client(credentials = credentials, project = credentials.project_id)
+# dataset, table = "dbt_smaitra", "landing_grocery_flyer_items"
 
-# truncate
-qry = "truncate table " + dataset + "." + table 
-res = client.query(qry).result()
+# # truncate
+# qry = "truncate table " + dataset + "." + table 
+# res = client.query(qry).result()
 
-# ID
-qry = "select row_number() over() id, * from flyer_df"
-load_df = duckdb.query(qry).df()
+# # ID
+# qry = "select row_number() over() id, * from flyer_df"
+# load_df = duckdb.query(qry).df()
 
-# load
-tbl = client.dataset(dataset).table(table)
-job = client.load_table_from_dataframe(load_df, tbl)
+# # load
+# tbl = client.dataset(dataset).table(table)
+# job = client.load_table_from_dataframe(load_df, tbl)
 
 # stats
 job.result()
